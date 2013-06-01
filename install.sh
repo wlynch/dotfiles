742114cfc0c69498b63e6f5299c6e898a37162cf
Billy Lynch <wlynch92@gmail.com>
2013-05-31 22:55:59 -0400

    add submodule support

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ if [ ! -d "$SRCDIR" ] ; then
   echo "$HOME/dotfiles does not exist. Exiting"
 fi
 
+cd $SRCDIR
+git submodules init
+git submodules update
+
 for FILE in `find "$SRCDIR" -type f` ; do
   NAME=`echo "$FILE" | sed -e "s|^$SRCDIR\/||"`
   if [[ "$NAME" != "$SRC" ]] ; then
